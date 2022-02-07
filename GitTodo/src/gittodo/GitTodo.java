afb1668e0b7ae6df3176ff6e74a80d3cbe8b81a0
awkward-abi <79824839+awkward-abi@users.noreply.github.com>
2022-02-07 19:55:55 +0800

    Fixed conflict in GitTodo.java

--- GitTodo/src/gittodo/GitTodo.java
+++ GitTodo/src/gittodo/GitTodo.java
@@ -15,8 +15,6 @@ public class GitTodo {
         String name = input.nextLine();
         System.out.println("Welcome "+name+"\nPlease enter your task for today");
         String task = input.nextLine();
-
-        System.out.println("Hello world!"); // this is my comment 
         
         System.out.println("Hello Git");
     }
